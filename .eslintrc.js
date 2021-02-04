d40356eb52ce13fc9c32e6530971b61463eb7834
Alexandre Cailler <69685205+alex-cailler@users.noreply.github.com>
2021-02-04 17:13:29 +0100

    feat: add ci rules (#1)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,24 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
   },
+  // ignorePatterns: ['/docs', '/test'],
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended'
   ],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  globals: {
+    $nuxt: true,
+  },
   parserOptions: {
     parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
