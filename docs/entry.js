ec6637a4f0ca076e40862a0125df4317011d3bce
lulmairine <alex02.cailler@gmail.com>
2021-02-04 21:50:16 +0100

    feat: add ci rules

---
+++ docs/entry.js
@@ -0,0 +1,22 @@
+
+    window.reactComponents = {};
+
+    window.vueComponents = {};
+
+  
+      import Vue from 'vue/dist/vue.js';
+
+      window.Vue = Vue;
+
+
+      import VueWrapper from '../node_modules/better-docs/lib/vue-wrapper.js';
+
+      window.VueWrapper = VueWrapper;
+
+    
+    import './styles/reset.css';
+
+    import './styles/iframe.css';
+
+  import Component0 from '../src/components/ProgressBar.vue';
+vueComponents['ProgressBar'] = Component0;
