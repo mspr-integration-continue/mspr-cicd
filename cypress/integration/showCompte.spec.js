646353e78b0d7dede9cbd6ce181a0aa2bc038f9d
Breton <bryan.breton@epsi.fr>
2021-02-04 18:35:44 +0100

    fix(users): add spaces

--- cypress/integration/showCompte.spec.js
+++ cypress/integration/showCompte.spec.js
@@ -1,7 +1,7 @@
 describe('Compte test', () => {
     it('First test', () => {
-        cy.visit('http://localhost:8080')
+        cy.visit('http://localhost:8081')
         cy.get("#123456").click()
         cy.location('href').should('include', '/about/123456')
     })
-  })
+})
