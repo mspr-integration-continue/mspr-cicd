447dea7e2496270ed63c1dcde44e8516c1129164
AxelBlusseau <axelbluss@gmail.com>
2021-02-15 10:05:57 +0100

    feat(TU): Init TU

--- src/store/users/index.js
+++ src/store/users/index.js
@@ -1,6 +1,10 @@
 import state from './users.states';
+import mutations from './users.mutations';
+import actions from './users.actions';
 
 export default {
   namespaced: true,
   state,
+  mutations,
+  actions,
 };
