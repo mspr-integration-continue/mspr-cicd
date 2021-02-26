401417720fabec6946531010b15fa579e90479c4
AxelBlusseau <axelbluss@gmail.com>
2021-02-26 21:35:33 +0100

    feat(codacy): test

--- src/main.js
+++ src/main.js
@@ -3,8 +3,22 @@ import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 import store from './store';
+import { Notifier } from '@airbrake/browser';
 
 Vue.config.productionTip = false;
+
+var airbrake = new Notifier({
+  environment: 'production',
+  projectId: 324964,
+  projectKey: '9849b8c4c65f52021ec7a83366b2fd00',
+});
+
+Vue.config.errorHandler = function (err, vm, info) {
+  airbrake.notify({
+    error: err,
+    params: { info: info },
+  });
+};
 
 new Vue({
   router,
