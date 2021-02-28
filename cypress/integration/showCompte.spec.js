2301e3b66d3cfa07bc76f742954a300a8aabc4eb
Breton <bryan.breton@epsi.fr>
2021-02-28 15:38:13 +0100

    feat(tests): change TU for the new version

--- cypress/integration/showCompte.spec.js
+++ cypress/integration/showCompte.spec.js
@@ -1,7 +1,12 @@
 describe('Compte test', () => {
     it('First test', () => {
-        cy.visit('http://localhost:8081')
+        cy.visit('http://localhost:8080')
         cy.get("#123456").click()
         cy.location('href').should('include', '/about/123456')
+        const solde1 = cy.get('#solde')
+        cy.get('input[type="number"]').type('1000').trigger('change')
+        cy.get('#button-addon2').click()
+        const solde2 = cy.get('#solde')
+        expect(solde1).to.not.equal(solde2)
     })
 })
